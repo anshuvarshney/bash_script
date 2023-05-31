ad8b9d4a15f0de6929cbb49192856a5eef798ee2
deVopsAnshu <115215127+anshuvarshney@users.noreply.github.com>
2023-05-31 21:52:39 +0530

    Update File_removal.sh

--- File_removal.sh
+++ File_removal.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-folder_path="/home/anshu/sample_project"
+folder_path="/home/anshu/sample_project"  #change the directory path
 three_days_ago=$(date -d "-3 days" +%s)
 
 # Iterate over the files in the folder
-for file in $/home/anshu/sample_project/*; do
+for file in $/home/anshu/sample_project/*; do  #change the directory path
     # Check if the file is a regular file and not a directory
     if [[ -f "$file" ]]; then
         # Get the last modified time of the file
